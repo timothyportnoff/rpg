ccba1f8c545c855ca75fc5b6d71e5cdb8aa4ba96
Timothy <portnoff_0795391@csci4x.com>
2022-04-08 20:20:25 -0700

    Added a broken getter and setter

--- point.cc
+++ point.cc
@@ -4,25 +4,26 @@
 using namespace std;
 
 struct Point {
-    float x, y;
-    //default
-    //Point() = default;
-    //Better constructor
-    Point() {
-	    x = 0;
-	    y = 0;
-    }
-    //nondefault
-    Point(int new_x, int new_y) {
-        x = new_x;
-        y = new_y;
-    }
-    void set_x(int x) { this->x = x; }
-    void set_y(int y) { this->y = y; }
-    ~Point () {}
+	float x, y;
+	//default
+	//Point() = default;
+	//Better constructor
+	Point() {
+		x = 0;
+		y = 0;
+	}
+	//nondefault
+	Point(int new_x, int new_y) {
+		x = new_x;
+		y = new_y;
+	}
+	public:
+	void set_x(int x) { this->x = x; }
+	void set_y(int y) { this->y = y; }
+	~Point () {}
 };
 ostream& operator <<(ostream& outs, const Point& p) {
-    outs << setprecision(2) << fixed;
-    outs << "(" << p.x << "," << p.y << ")";
-    return outs;
+	outs << setprecision(2) << fixed;
+	outs << "(" << p.x << "," << p.y << ")";
+	return outs;
 }
