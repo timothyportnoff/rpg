9a94a676d2e5324dcb71214bef22878620eb63cd
timothyportnoff <timothyportnoff@gmail.com>
2023-03-06 18:15:03 -0800

    Changed point to class instead of struct

--- point.cc
+++ point.cc
@@ -3,31 +3,29 @@
 #include <iomanip>
 using namespace std;
 
-struct Point {
-	int x = 0; 
-	int y = 0;
-	//default
-	//Point() = default;
-	//Better constructor
-	Point() {
-		x = 0;
-		y = 0;
-	}
-	//nondefault
-	Point(int new_x, int new_y) {
-		x = new_x;
-		y = new_y;
-	}
+class Point {
+	private:
+		int x = 0; 
+		int y = 0;
 	public:
-	int get_x() const { return x; }
-	void set_x(int x) { this->x = x; }
-	int get_y() const { return y; }
-	void set_y(int y) { this->y = y; }
-	~Point () {}
+		Point() {
+			x = 0;
+			y = 0;
+		}
+		Point(int new_x, int new_y) {
+			x = new_x;
+			y = new_y;
+		}
+		public:
+		int get_x() const { return x; }
+		void set_x(int x) { this->x = x; }
+		int get_y() const { return y; }
+		void set_y(int y) { this->y = y; }
+		~Point () {}
+
+		ostream& operator <<(ostream& outs, const Point p) {
+			//outs << setprecision(2) << fixed; //Set precision
+			outs << "(" << p.x << ", " << p.y << ")";
+			return outs;
+		}
 };
-ostream& operator <<(ostream& outs, const Point p) {
-	//outs << setprecision(2) << fixed;
-	outs << "Position X: " << p.x << "\tY: " << p.y << endl; 
-	//outs << "(" << p.x << "," << p.y << ")";
-	return outs;
-}
